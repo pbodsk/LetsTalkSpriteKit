ab741898214a6eb39f4dbd031f6f0a9d42ac4da9
pbodsk <me@peterbodskov.dk>
2015-02-03 21:07:46 +0100

    background bars are go

--- LetsTalkSpriteKit/BackgroundNode.swift
+++ LetsTalkSpriteKit/BackgroundNode.swift
@@ -16,45 +16,60 @@ class BackgroundNode: SKSpriteNode {
     let redBar: SKSpriteNode = SKSpriteNode(color: UIColor.redColor(), size: CGSizeZero)
     
     func createBouncingBarsInView(view: SKView) {
-        let barSize = CGSize(width: view.frame.width, height: 20)
-        let barActionDown = SKAction.moveToY(CGRectGetMinY(view.frame), duration: 1.5)
-        let barActionUp = SKAction.moveToY(CGRectGetMaxY(view.frame), duration: 1.5)
-
-        /*
-        let barDelay = SKAction.waitForDuration(1.0)
-        */
-        let barActionSequece = SKAction.sequence([barActionDown, barActionUp])
         
-        let barActionForever = SKAction.repeatActionForever(barActionSequece)
-        /*
-        let barActionBlueSequence = SKAction.sequence([barDelay, barActionGreenSequece])
-        let barActionBlueForever = SKAction.repeatActionForever(barActionBlueSequence)
-*/
+        let barSize = CGSize(width: view.frame.width, height: 20)
         greenBar.position = CGPoint(x: CGRectGetMidX(view.frame), y: 100.0)
         greenBar.size = barSize
         greenBar.zPosition = 10
-        greenBar.runAction(barActionForever)
         self.addChild(greenBar)
         
         blueBar.position = CGPoint(x: CGRectGetMidX(view.frame), y: 150.0)
         blueBar.size = barSize
         blueBar.zPosition = 11
-//        blueBar.runAction(SKAction.waitForDuration(1.0))
-        blueBar.runAction(barActionForever)
         self.addChild(blueBar)
+        
         
         yellowBar.position = CGPoint(x: CGRectGetMidX(view.frame), y: 200.0)
         yellowBar.size = barSize
         yellowBar.zPosition = 12
-//        yellowBar.runAction(barActionForever)
         self.addChild(yellowBar)
         
         redBar.position = CGPoint(x: CGRectGetMidX(view.frame), y: 250.0)
         redBar.size = barSize
         redBar.zPosition = 13
-  //      redBar.runAction(barActionForever)
+
         self.addChild(redBar)
         
-       // self.runAction(barActionGreenForever)
+        self.bounceAndBlinkLikeAManiacInView(view)
+    }
+    
+    func bounceAndBlinkLikeAManiacInView(view: SKView) {
+        let barPath = CGPathCreateMutable()
+        CGPathMoveToPoint(barPath, nil, CGRectGetMinX(view.frame), CGRectGetMinY(view.frame))
+        CGPathAddLineToPoint(barPath, nil, CGRectGetMinX(view.frame), 100)
+        CGPathCloseSubpath(barPath)
+        let barPathAction = SKAction.followPath(barPath, duration: 2.0)
+        let barHide = SKAction.fadeOutWithDuration(1.0)
+        let barShow = SKAction.fadeInWithDuration(1.0)
+        let greenBarSequence = SKAction.sequence([barHide, barShow])
+        let barPathActionForever = SKAction.repeatActionForever(barPathAction)
+        let greenBarHideShowForever = SKAction.repeatActionForever(greenBarSequence)
+        greenBar.runAction(barPathActionForever)
+        greenBar.runAction(greenBarHideShowForever)
+        
+        blueBar.runAction(barPathActionForever)
+        let blueBarSequence = SKAction.sequence([SKAction.waitForDuration(1.0), barHide, barShow])
+        let blueBarHideShowForever = SKAction.repeatActionForever(blueBarSequence)
+        blueBar.runAction(blueBarHideShowForever)
+
+        yellowBar.runAction(barPathActionForever)
+        let yellowBarSequence = SKAction.sequence([SKAction.waitForDuration(1.5), barHide, barShow])
+        let yellowBarHideShowForever = SKAction.repeatActionForever(yellowBarSequence)
+        yellowBar.runAction(yellowBarHideShowForever)
+
+        redBar.runAction(barPathActionForever)
+        let redBarSequence = SKAction.sequence([SKAction.waitForDuration(2.0), barHide, barShow])
+        let redBarHideShowForever = SKAction.repeatActionForever(redBarSequence)
+        redBar.runAction(redBarHideShowForever)
     }
 }
